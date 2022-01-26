46dbb016e34d577b54cca3a5560624e712a34349
crislav10 <cristianlav10@gmail.com>
2022-01-25 19:51:04 -0500

    React Context

--- src/AppUI.js
+++ src/AppUI.js
@@ -7,47 +7,43 @@ import { CreateTodoButton } from "./components/CreateTodoButton";
 import {TodoContext} from "./TodoContex";
 
 function AppUI(){
- return (
-    <React.Fragment>
-        <TodoCounter/>
+    const {
+        error,
+        loading,
+        todos,
+        searchValue,
+        completeTodo,
+        deleteTodo} =React.useContext(TodoContext);
 
-        <TodoSearch />
+    return (
+        <React.Fragment>
+            <TodoCounter/>
 
-        <TodoContext.Consumer>
-            {
-                ({
-                    error,
-                    loading,
-                    todos,
-                    searchValue,
-                    completeTodo,
-                    deleteTodo}) =>{
-                    return(
-                        <TodoList>
-                        {error && <p>Error</p>}
-                        {loading && <p>Estamos Cargando</p>}
-                        {(!loading && !todos.lenght) && <p>Crea tu primer Todo.</p>}
+            <TodoSearch />
 
-                        {todos.filter(todo=>todo.text.toLowerCase().includes(searchValue.toLowerCase()))
-                        .map(todo=>(
-                        <TodoItem 
-                        key={todo.text} 
-                        text={todo.text} 
-                        completed={todo.completed}
-                        onComplete={completeTodo}
-                        onDelete={deleteTodo}
-                        />))}
-                        </TodoList> 
-                        
-                    );
-                }
-            }
-        </TodoContext.Consumer>
-        
-        <CreateTodoButton />
-        
-  </React.Fragment>
- );
+            
+                    
+            <TodoList>
+                {error && <p>Error</p>}
+                {loading && <p>Estamos Cargando</p>}
+                {(!loading && todos.lenght===0) && <p>Crea tu primer Todo.</p>}
+
+                {todos.filter(todo=>todo.text.toLowerCase().includes(searchValue.toLowerCase()))
+                .map(todo=>(
+                <TodoItem 
+                    key={todo.text} 
+                    text={todo.text} 
+                    completed={todo.completed}
+                    onComplete={completeTodo}
+                    onDelete={deleteTodo}
+                />))}
+            </TodoList> 
+                      
+            
+            <CreateTodoButton />
+            
+    </React.Fragment>
+    );
 }
 
 export {AppUI};
--- src/components/TodoCounter.js
+++ src/components/TodoCounter.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { TodoContext } from "../TodoContex";
 import './components.css';
 
-function TodoCounter({total,completed}){
+function TodoCounter(){
+    const {totalTodos,completed}=React.useContext(TodoContext);
     return(
-        <h2 className="TodoCounter">Has completado {completed} de {total} TODOs</h2>
+        <h2 className="TodoCounter">Has completado {completed} de {totalTodos} TODOs</h2>
     )
 }
 
